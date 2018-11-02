93de5cfa7cb358a592420260de883e78c21eebbb
Taimur Shah <tshah@scu.edu>
2018-11-02 16:46:35 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,20 @@ const codes = [
   "a"
 ];
 
+let index = 0
+
 function init() {
-  
+  document.addEventListener("keydown", function(e) {
+    let key = e.key;
+    if (key = codes[index]) {
+        index++;
+        if (index === codes.length) {
+            alert("Yeah bitch");
+            
+            index = 0;
+        }
+    } else { 
+        index = 0;
+    }
+})
 }
